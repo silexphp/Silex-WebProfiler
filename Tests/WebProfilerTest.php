ffb821fcdf45d1c557b477728207edf945efbf72
Jérôme Tamarelle <jerome@tamarelle.net>
2015-05-12 10:38:44 +0200

    Add functional tests

---
+++ Tests/WebProfilerTest.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of the Silex framework.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Silex\Provider\Tests;
+
+use Silex\Application;
+use Silex\WebTestCase;
+use Silex\Provider;
+
+class WebProfilerTest extends WebTestCase
+{
+    public function createApplication()
+    {
+        $app = new Application();
+
+        // Service providers
+        $app->register(new Provider\HttpFragmentServiceProvider());
+        $app->register(new Provider\ServiceControllerServiceProvider());
+        $app->register(new Provider\TwigServiceProvider());
+        $app->register(new Provider\RoutingServiceProvider());
+
+        $app->register(new Provider\WebProfilerServiceProvider(), array(
+            'profiler.cache_dir' => __DIR__.'/cache/profiler',
+        ));
+
+        // Test Config
+        $app['debug'] = true;
+        unset($app['exception_handler']);
+        $app['session.test'] = true;
+
+        // Test route
+        $app->get('/', function () {
+            return '<body>OK</body>';
+        });
+
+        return $app;
+    }
+
+    public function testRun()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertTrue($client->getResponse()->isOk(), 'Response successful');
+        $this->assertCount(1, $crawler->filter('.sf-toolbar'), 'WDT is inserted.');
+        $this->assertTrue($client->getResponse()->headers->has('X-Debug-Token'), 'Profile ID');
+        $this->assertTrue($client->getResponse()->headers->has('X-Debug-Token-Link'), 'Profiler link');
+
+        $link = $client->getResponse()->headers->get('X-Debug-Token-Link');
+
+        $crawler = $client->request('GET', $link);
+        $this->assertTrue($client->getResponse()->isOk(), 'Profile accessible');
+
+        $client->followRedirects(true);
+        $crawler = $client->request('GET', '/_profiler/');
+        $this->assertTrue($client->getResponse()->isOk(), 'Profiler accessible');
+    }
+}
